ef07726b297e65b466155f223ad59888b41fea5b
Ben Morrison <mrbmorrison@gmail.com>
2013-10-23 17:04:16 +0100

    refactoring of elo

--- global.js
+++ global.js
@@ -156,87 +156,26 @@ if(Meteor.isServer){
 			var winner = Meteor.users.find({_id: winnerId}).fetch()[0];			
 			var loser = Meteor.users.find({_id: loserId}).fetch()[0];
 
-			var EloRating, elo, girlA, girlB, ratingA, ratingB, results;
+			function expectedScore(playerRating, opponentRating){
+				return 1 / (1 + Math.pow(10, (opponentRating - playerRating) / 400));
+			} 
 
-			  EloRating = (function() {
+			function eloRating(playerRating, expectedScore, score){
+				return parseInt(playerRating + Math.min(16, (10 * (score - expectedScore))));
+			}
 
-			    function EloRating(ratingA, ratingB, scoreA, scoreB) {
-			     	var expectedScores, newRatings;
-			      this.KFACTOR = 32;
-			      this._ratingA = ratingA;
-			      this._ratingB = ratingB;
-			      this._scoreA = scoreA;
-			      this._scoreB = scoreB;
-			      expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
-			      this._expectedA = expectedScores.a;
-			      this._expectedB = expectedScores.b;
-			      newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
-			      this._newRatingA = newRatings.a;
-			      this._newRatingB = newRatings.b;
-			    }
-
-			    EloRating.prototype.setNewSetings = function(ratingA, ratingB, scoreA, scoreB) {
-			      var expectedScores, newRatings;
-			      this._ratingA = ratingA;
-			      this._ratingB = ratingB;
-			      this._scoreA = scoreA;
-			      this._scoreB = scoreB;
-			      expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
-			      this._expectedA = expectedScores.a;
-			      this._expectedB = expectedScores.b;
-			      newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
-			      this._newRatingA = newRatings.a;
-			      return this._newRatingB = newRatings.b;
-			    };
-
-			    EloRating.prototype.getNewRatings = function() {
-			      var ratings;
-			      return ratings = {
-			        a: Math.round(this._newRatingA),
-			        b: Math.round(this._newRatingB)
-			      };
-			    };
-
-			    EloRating.prototype._getExpectedScores = function(ratingA, ratingB) {
-			      var expected, expectedScoreA, expectedScoreB;
-			      expectedScoreA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
-			      expectedScoreB = 1 / (1 + Math.pow(10, (ratingA - ratingB) / 400));
-			      return expected = {
-			        a: expectedScoreA,
-			        b: expectedScoreB
-			      };
-			    };
-
-			    EloRating.prototype._getNewRatings = function(ratingA, ratingB, expectedA, expectedB, scoreA, scoreB) {
-			      var newRatingA, newRatingB, ratings;
-			      newRatingA = ratingA + Math.min(this.KFACTOR, (10 * (scoreA - expectedA)));
-			      newRatingB = ratingB + Math.min(this.KFACTOR, (10 * (scoreB - expectedB)));
-			      return ratings = {
-			        a: newRatingA,
-			        b: newRatingB
-			      };
-			    };
-
-			    return EloRating;
-
-			  })();
-
-			  elo = new EloRating();
-
-			  elo.setNewSetings(parseInt(winner.profile.rating), parseInt(loser.profile.rating), 1, 0);
-
-			  results = elo.getNewRatings();
-
+			var winnerNewRating = eloRating(winner.profile.rating, expectedScore(winner.profile.rating, loser.profile.rating), 1);
+			var loserNewRating = eloRating(loser.profile.rating, expectedScore(loser.profile.rating, winner.profile.rating), 0);
 			
 			Meteor.users.update(winner._id, {
 				$set: {
-					'profile.rating':  results.a
+					'profile.rating':  winnerNewRating
 				} 
 			});
-			
+
 			Meteor.users.update(loser._id, {
 				$set: {
-					'profile.rating': results.b
+					'profile.rating': loserNewRating
 				} 
 			});
 			
